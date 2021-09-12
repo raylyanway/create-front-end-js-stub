52ed44a3cb42e380c12db7f901312dd2453e5bb5
iLya Raylyan <IRaylyan@alfabank.ru>
2021-09-12 18:40:41 +0300

    feat(*): init project

---
+++ src/reportWebVitals.ts
@@ -0,0 +1,17 @@
+import { ReportHandler } from "web-vitals";
+
+type ReportWebVitals = (onPerfEntry?: ReportHandler | undefined) => void;
+
+const reportWebVitals: ReportWebVitals = (onPerfEntry?: ReportHandler) => {
+  if (onPerfEntry && onPerfEntry instanceof Function) {
+    import("web-vitals").then(({ getCLS, getFID, getFCP, getLCP, getTTFB }) => {
+      getCLS(onPerfEntry);
+      getFID(onPerfEntry);
+      getFCP(onPerfEntry);
+      getLCP(onPerfEntry);
+      getTTFB(onPerfEntry);
+    });
+  }
+};
+
+export default reportWebVitals;
